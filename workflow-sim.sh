7119d44af15d5c083a6fcd04e7bfa6864e3b4f67
hettling <hannes.hettling@gmail.com>
2015-11-09 17:42:05 +0100

    adding alns, trying to get dir structure right

--- workflow-sim.sh
+++ workflow-sim.sh
@@ -12,48 +12,58 @@
 # Note that running this script will take considerable time and resources,
 # so it is best to run it on a cluster computer.
 
-# Note that this script must be called from the working directory of a 
-# previous (completed!) SUPERSMART run.
+# Note that this script needs the alignment files and final tree from
+# a prevously completed supersmart run.
 
 # A directory name to store all simulated data and analysis results can be provided
-# as an argument, if not given it defaults to 'simulations/' within the 
-# current workind directory
+# as an argument, if not given it defaults to 'results/simulations/' 
 
-# parse command-line arguments for simulation directory name
-if [ -z "$1" ]
-        then
-        SIMDIR='simulations'
-else
-        SIMDIR=$1
+# parse command-line arguments for simulation directory 
+SIMNAME=$1
+if [ -z "$SIMNAME" ]; then       
+        SIMNAME='simulations'
 fi
 
+# define dirs and directories
+WORKDIR="$PWD/results/$SIMNAME" 
+
 # make directory to store all files for simulation analysis
-mkdir $SIMDIR
+if [ ! -d $WORKDIR ]; then
+	mkdir $WORKDIR
+fi
+
+INTREE="$PWD/data/tree/final.nex"
+FOSSILS="$PWD/data/fossils/fossils.tsv"
+
+# make file listing original alignments
+ALNS=$WORKDIR/aligned.txt
+for i in $( ls data/alignments/*.fa ); do echo $PWD/$i; done > $ALNS
 
 # copy files from previous SUPERSMART run necessary to replicate tree and data
-cp final.nex $SIMDIR
-cp aligned.txt $SIMDIR
-cp fossils.tsv $SIMDIR
-cd $SIMDIR
+cp $INTREE $WORKDIR
+cp $FOSSILS $WORKDIR
+cd $WORKDIR
+
+echo "$WORKDIR/replicate.log"
 
 # Replicate the dataset (final tree, taxa table and alignments)
-smrt-utils replicate -t final.nex -f nexus -a aligned.txt -l replicate.log -v
+smrt-utils replicate -t $INTREE -f nexus -a $ALNS -l "$WORKDIR/replicate.log" -v
 # insert simulated sequences and possible artificial taxa into the database
 # prefix for sequence accessions is the simulation directory
-smrt-utils dbinsert -s aligned-replicated.txt -t taxa-replicated.tsv -p $SIMDIR -v
+smrt-utils dbinsert -s aligned-replicated.txt -t taxa-replicated.tsv -p $SIMNAME -v -w $WORKDIR
 
-# Rerun the supersmart pipeline in the directory of the replicated dataset
-smrt orthologize -i aligned-smrt-inserted.txt
-smrt bbmerge -t taxa-replicated.tsv -a merged.txt
-smrt bbinfer -i exabayes -s supermatrix.phy
-smrt bbreroot -b backbone.dnd -t taxa-replicated.tsv
-smrt bbcalibrate -t backbone-rerooted.dnd -f fossils.tsv
-smrt consense -i chronogram.dnd
-smrt bbdecompose -b consensus.nex -a aligned-smrt-inserted.txt -t taxa-replicated.tsv
-smrt clademerge --enrich
-smrt cladeinfer --ngens=15_000_000 --sfreq=1000 --lfreq=1000
-smrt cladegraft
+# run the supersmart pipeline in the directory of the replicated dataset
+smrt orthologize -i aligned-smrt-inserted.txt -w $WORKDIR
+smrt bbmerge -t taxa-replicated.tsv -a merged.txt -w $WORKDIR
+smrt bbinfer -i exabayes -s supermatrix.phy -w $WORKDIR
+smrt bbreroot -b backbone.dnd -t taxa-replicated.tsv -w $WORKDIR
+smrt bbcalibrate -t backbone-rerooted.dnd -f $FOSSILS
+smrt consense -i chronogram.dnd -w $WORKDIR
+smrt bbdecompose -b consensus.nex -a aligned-smrt-inserted.txt -t taxa-replicated.tsv -w $WORKDIR
+smrt clademerge --enrich -w $WORKDIR
+smrt cladeinfer --ngens=30_000_000 --sfreq=1000 --lfreq=1000 -w $WORKDIR
+smrt cladegraft -w $WORKDIR
 
 # clean database from artificial sequences
-sqlite3 $SUPERSMART_HOME/data/phylota.sqlite 'delete from seqs where acc like "$SIMDIR%"'
-sqlite3 $SUPERSMART_HOME/data/phylota.sqlite 'delete from nodes_194 where common_name like "$SIMDIR%"'
+sqlite3 $SUPERSMART_HOME/data/phylota.sqlite 'delete from seqs where acc like "$WORKDIR%"'
+sqlite3 $SUPERSMART_HOME/data/phylota.sqlite 'delete from nodes_194 where common_name like "$WORKDIR%"'
