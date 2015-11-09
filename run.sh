73733c6601e1b121aae3854c726a3e70358acfff
hettling <hannes.hettling@gmail.com>
2015-11-10 00:01:40 +0100

    using default tempdir

--- run.sh
+++ run.sh
@@ -9,8 +9,6 @@ module load bioinfo-tools
 module load blast/2.2.29+
 export PATH=$PATH:/home/hannesh/SUPERSMART/tools/bin/
 export PATH=$PATH:/home/hannesh/SUPERSMART/src/supersmart/script
-export TMPDIR="/home/hannesh/glob/tmp/$1"
-mkdir $TMPDIR
 
 sh workflow-sim.sh $1
 
