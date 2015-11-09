18f1209ff03dbfea579bcd4490df3407af62f353
hettling <hannes.hettling@gmail.com>
2015-11-09 17:46:20 +0100

    adding date to simdir

--- runall.sh
+++ runall.sh
@@ -1,1 +1,1 @@
-for i in {1..5}; do sbatch -o primates-sim-$i.out -J primates-sim-$i run.sh simulations-$i; done
+for i in {1..5}; do sbatch -o primates-sim-$i.out -J primates-sim-$i run.sh `date +%Y-%m-%d`-sim-$i; done
