2626a005281c42a8903721d975dddac728a7d0f6
hettling <hannes.hettling@gmail.com>
2015-11-11 01:14:51 +0100

    added plotting script

---
+++ plot-alignments.sh
@@ -0,0 +1,13 @@
+
+# get stats of original alignment
+ORIG_ALNS='results/aligned-orig.txt'
+ORIG_STATS='results/alignment-stats-orig.tsv'
+for i in $( ls data/alignments/*.fa ); do echo $PWD/$i; done > $ORIG_ALNS
+
+smrt-utils alnstats -a $ORIG_ALNS -o $ORIG_STATS
+
+STATSFILES=$(ls results/2015-11-10-sim-*/alignment-stats.tsv)
+
+echo $ORIG_STATS $STATSFILES
+
+Rscript script/plot-aln-stats.R alignment-stats-original.tsv $ORIG_STATS $STATSFILES
