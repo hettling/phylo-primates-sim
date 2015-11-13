1656d2bc6ad81e82cc915b2ab316af48b231bb62
hettling <hannes.hettling@gmail.com>
2015-11-13 10:39:00 +0100

    make markergraph

--- script/workflow-supersmart.sh
+++ script/workflow-supersmart.sh
@@ -15,9 +15,9 @@ fi
 WORKDIR="$PWD/results/$SIMNAME" 
 
 # run the supersmart pipeline in the directory of the replicated dataset
-export SUPERSMART_BACKBONE_MAX_DISTANCE="0.1"
+export SUPERSMART_BACKBONE_MAX_DISTANCE="0.2"
 export SUPERSMART_BACKBONE_MIN_COVERAGE="3"
-export SUPERSMART_BACKBONE_MAX_COVERAGE="5"
+export SUPERSMART_BACKBONE_MAX_COVERAGE="3"
 export SUPERSMART_CLADE_MAX_DISTANCE="0.2"
 export SUPERSMART_CLADE_MIN_DENSITY="0.5"
 export SUPERSMART_CLADE_MIN_COVERAGE="2"
@@ -25,17 +25,20 @@ export SUPERSMART_CLADE_MAX_COVERAGE="10"
 
 FOSSILS='fossils.tsv'
 BACKBONE='backbone-examl.dnd'
+SUPERMATRIX='supermatrix.phy'
 
 smrt orthologize -i aligned-smrt-inserted.txt -w $WORKDIR
-smrt bbmerge -t taxa-replicated.tsv -a merged.txt -w $WORKDIR
-smrt bbinfer -i examl -b 100 -s supermatrix.phy -o $BACKBONE -w $WORKDIR
+smrt bbmerge -t taxa-replicated.tsv -a merged.txt -o $SUPERMATRIX -w $WORKDIR
+smrt-utils markergraph -i markers-backbone.tsv -o markers-backbone.dot
+
+smrt bbinfer -i examl -b 100 -s supermatrix.phy -m -o $BACKBONE -w $WORKDIR
 smrt bbreroot -b $BACKBONE -t taxa-replicated.tsv -w $WORKDIR
 smrt bbcalibrate -t backbone-rerooted.dnd -f $FOSSILS -w $WORKDIR
 smrt consense -i chronogram.dnd -w $WORKDIR
 
 smrt bbdecompose -b consensus.nex -a aligned-smrt-inserted.txt -t taxa-replicated.tsv -w $WORKDIR
 smrt clademerge --enrich -w $WORKDIR
-smrt cladeinfer --ngens=30_000_000 --sfreq=1000 --lfreq=1000 -w $WORKDIR
+smrt cladeinfer --ngens=15_000_000 --sfreq=1000 --lfreq=1000 -w $WORKDIR
 smrt cladegraft -w $WORKDIR
 
 # clean database from artificial sequences
