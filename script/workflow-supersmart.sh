170a9cc80a7363f20eb24b98361cd13392da466a
hettling <hannes.hettling@gmail.com>
2015-11-16 22:54:00 +0100

    updated

--- script/workflow-supersmart.sh
+++ script/workflow-supersmart.sh
@@ -23,39 +23,40 @@ export SUPERSMART_CLADE_MIN_DENSITY="0.5"
 export SUPERSMART_CLADE_MIN_COVERAGE="2"
 export SUPERSMART_CLADE_MAX_COVERAGE="10"
 
-FOSSILS='fossils.tsv'
-BACKBONE='backbone-examl.dnd'
-SUPERMATRIX='supermatrix.phy'
+FOSSILS="fossils.tsv"
+BACKBONE="backbone-examl.dnd"
+SUPERMATRIX="supermatrix.phy"
+
 
 if [ ! -e "$WORKDIR/merged.txt" ]; then
-    smrt orthologize -i aligned-smrt-inserted.txt -w $WORKDIR -l orthologize.log
+    smrt orthologize -i $WORKDIR/aligned-smrt-inserted.txt -w $WORKDIR -l orthologize.log
 fi
 
 if [ ! -e "$WORKDIR/supermatrix.phy" ]; then
-    smrt bbmerge -t taxa-replicated.tsv -a merged.txt -o $SUPERMATRIX -w $WORKDIR -l bbmerge.log
+    smrt bbmerge -t $WORKDIR/taxa-replicated.tsv -a merged.txt -o $SUPERMATRIX -w $WORKDIR -l bbmerge.log
 fi
 
 if [ ! -e "$WORKDIR/markers-backbone.dot" ]; then
-    smrt-utils markergraph -i markers-backbone.tsv -o markers-backbone.dot -w $WORKDIR
+    smrt-utils markergraph -i $WORKDIR/markers-backbone.tsv -o markers-backbone.dot -w $WORKDIR
 fi
 
 if [ ! -e "$WORKDIR/$BACKBONE" ]; then
-    smrt bbinfer -i examl -b 100 -s supermatrix.phy -m -o $BACKBONE -w $WORKDIR -l bbinfer.log
+    smrt bbinfer -i examl -b 100 -s $WORKDIR/supermatrix.phy -m -o $BACKBONE -w $WORKDIR -l bbinfer.log
 fi
 
 if [ ! -e "$WORKDIR/backbone-rerooted.dnd" ]; then
-    smrt bbreroot -b $BACKBONE -t taxa-replicated.tsv -p tree-replicated.dnd -w $WORKDIR
+    smrt bbreroot -b $BACKBONE -t $WORKDIR/taxa-replicated.tsv -p $WORKDIR/tree-replicated.dnd -w $WORKDIR
 fi
 
 if [ ! -e "$WORKDIR/chronogram.dnd" ]; then
-    smrt bbcalibrate -t backbone-rerooted.dnd -f $FOSSILS -w $WORKDIR
+    smrt bbcalibrate -t $WORKDIR/backbone-rerooted.dnd -f $FOSSILS -w $WORKDIR
 fi
 
 if [ ! -e "$WORKDIR/consensus.nex" ]; then
-    smrt consense -i chronogram.dnd -w $WORKDIR
+    smrt consense -i $WORKDIR/chronogram.dnd -w $WORKDIR
 fi
 
-smrt bbdecompose -b consensus.nex -a aligned-smrt-inserted.txt -t taxa-replicated.tsv -w $WORKDIR
+smrt bbdecompose -b $WORKDIR/consensus.nex -a $WORKDIR/aligned-smrt-inserted.txt -t $WORKDIR/taxa-replicated.tsv -w $WORKDIR
 smrt clademerge --enrich -w $WORKDIR
 smrt cladeinfer --ngens=15_000_000 --sfreq=1000 --lfreq=1000 -w $WORKDIR
 smrt cladegraft -w $WORKDIR -o 'tree-reestimated.nex'
