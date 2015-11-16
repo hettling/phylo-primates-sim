0b3deab8728ea6e5308642161829469584a54f37
hettling <hannes.hettling@gmail.com>
2015-11-16 14:11:10 +0100

    updated

--- runall.sh
+++ runall.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-
-# date of sim can be passed as argument, otherwise it is current date
-SIMDATE=$1
-if [ -z $SIMDATE ]; then       
-    SIMDATE=`date +%Y-%m-%d`;
+# prefix for simulation dir passed as argument, otherwise set ti current date
+PREFIX=$1
+if [ -z $PREFIX ]; then       
+    PREFIX=`date +%Y-%m-%d`;
 fi
 
 # cluster queue submission 
-for i in {1..10}; do sbatch -o primates-mega-smrt-$i.out -J primates-mega-smrt-$i run.sh $SIMDATE-sim-$i; done
+for i in {1..5}; do sbatch -o sun-primates-sim-smrt-$i.out -J new-primates-sim-smrt-$i run.sh $PREFIX-$i; done
 
 # simple call
-#for i in {1..10}; do sh run.sh $SIMDATE-sim-$i; done
+#for i in {1..10}; do sh run.sh $PREFIX-sim-$i; done
