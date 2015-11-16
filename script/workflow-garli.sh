5012c45cb4500111c9b829ddcc124831d317bb9c
hettling <hannes.hettling@gmail.com>
2015-11-16 17:37:01 +0100

    adding workflows

---
+++ script/workflow-garli.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This script runs garli on a mega-matrix generated from
+# simulation data. This script should be called from the top-level directory  
+
+# load garli
+module load bioinfo-tools
+module load garli
+
+# parse command-line arguments for simulation directory 
+SIMNAME=$1
+
+# define files and directories
+WORKDIR="$PWD/results/$SIMNAME" 
+CONF="$PWD/script/garli.conf"
+
+# copy configuration file to working diorectory
+cp $CONF  $WORKDIR
+
+MEGAMATRIX="$WORKDIR/megamatrix.nex"
+
+cd $WORKDIR
+
+Garli -b $CONF
---
+++ script/workflow-mrbayes.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script runs mrbayes on a mega-matrix generated from
+# simulation data. This script should be called from the top-level directory  
+
+# load mrbayes
+module unload openmpi/1.6.5
+module load bioinfo-tools
+module load mrbayes
+
+# parse command-line arguments for simulation directory 
+SIMNAME=$1
+
+# define dirs
+WORKDIR="$PWD/results/$SIMNAME" 
+
+MEGAMATRIX="$WORKDIR/megamatrix.nex"
+
+mpirun -np 16 mb $MEGAMATRIX
+
