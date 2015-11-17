7af875000fa17751f70e5122855ff09d213a0bc6
hettling <hannes.hettling@gmail.com>
2015-11-17 11:20:03 +0100

    edited

--- plot-alignments.sh
+++ plot-alignments.sh
@@ -4,9 +4,9 @@ ORIG_ALNS='results/aligned-orig.txt'
 ORIG_STATS='results/alignment-stats-orig.tsv'
 #for i in $( ls data/alignments/*.fa ); do echo $PWD/$i; done > $ORIG_ALNS
 
-#smrt-utils alnstats -a $ORIG_ALNS -o $ORIG_STATS
+smrt-utils alnstats -a $ORIG_ALNS -o $ORIG_STATS
 
-STATSFILES=$(ls results/2015-11-10-sim-*/alignment-stats.tsv)
+STATSFILES=$(ls results/2015-11-15-halvan-sim-*/alignment-stats.tsv)
 
 echo $ORIG_STATS $STATSFILES
 
