24696d522ce955485b0797c85a8113197eb62d55
hettling <hannes.hettling@gmail.com>
2016-04-12 17:54:24 +0200

    making it work with phylogram

--- script/plot-treediffs.R
+++ script/plot-treediffs.R
@@ -1,14 +1,15 @@
 require('ape')
-source('mycopyloplot.R')
+source('mycophyloplot.R')
 
-rep <- read.tree('tree-replicated-sorted-labels.dnd')
-final <- read.tree('final-sorted-labels.dnd')
+rep <- read.tree('../results/2016-02-04-clusters/tree-replicated.dnd')
+final <- read.tree('../results/2016-02-04-clusters/tree-reestimated-pruned-sorted.dnd')
 
 # association matrix
 association <- cbind(final$tip.label, final$tip.label)
 
-# determine colors for associations
-tab <- read.table('clade-species.tsv')
+# determine colors for associations, need mapping between species and clades.
+# This can be done as follows: for i in $(ls -d clade*); do while read -r line; do echo $i $line; done < $i/species.tsv; done | grep -v superkingdom | cut -d " " -f 1,2
+tab <- read.table('../results/2016-02-04-clusters/clade-species.tsv')
 clades <- levels(tab[,1])
 clade_cols <- data.frame(clade=clades, color=rainbow(length(clades))[sample(seq_along(clades))]) #shuffle
 
@@ -21,9 +22,23 @@ colors <- unlist(sapply(association[,1], function(s) {
         return ('black')
 }))
 
-exemplars = scan('exemplars.tsv', what='character')
+# need exemplars file: cat markers-backbone.tsv  | cut -f 1 | grep -v superkingdom
+exemplars = scan('../results/2016-02-04-clusters/exemplars.tsv', what='character')
 
-pdf("nolab3.pdf", 20, 20)
-mycophyloplot(rep, final, space=100, gap=5, assoc=association, length.line=10, lwd=1, col=colors, type="cladogram", exemplars=exemplars, show.tip.label=F)
+
+
+#rem <- rep$tip.label[which(sapply(sapply(rep$tip.label, strsplit, "_"), `[[`, 1)!="Ateles")]
+
+#rep <- drop.tip(rep, rem)
+#final <- drop.tip(final, rem)
+#exemplars <- exemplars[which(! exemplars %in% rem)]
+
+## for figure 5b in main text
+pdf("trees-sidebyside-fig5b.pdf", 20, 20)
+mycophyloplot(rep, final, space=20, gap=-26, assoc=association, length.line=0.0, lwd=3, col=colors, exemplars=exemplars, show.tip.label=F, use.edge.length=T)
+#mycophyloplot(rep, final, space=100, gap=5, assoc=NULL, length.line=10, lwd=1, col=colors, exemplars=exemplars, show.tip.label=T, use.edge.length=T)
 dev.off()
+
+pdf("trees-sidebyside-fig")
+
 
